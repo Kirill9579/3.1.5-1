2ac36a0e8358f9a119accb9f3c19ca9b27287e74
Анатолий Андреев <anatoliiandreev19@gmail.com>
2022-08-16 14:05:56 +0300

    fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,21 +6,18 @@ import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	static RestTemplate restTemplate = new RestTemplate();
-
-	static String baseUrl = " http://94.198.50.185:7081/api/users";
+	static String baseUrl = "http://94.198.50.185:7081/api/users";
 
 	public static void main(String[] args) {
 
-		//SpringApplication.run(DemoApplication.class, args);
-
 		useExchangeMethodsOfRestTemplate();
 	}
-
 
 	private static void useExchangeMethodsOfRestTemplate() {
 
@@ -29,39 +26,52 @@ public class DemoApplication {
 
 		HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
 
-		getListUserByExchangeMethod(requestEntity);
+		ResponseEntity<List> responseEntity = getListUserByExchangeMethod(requestEntity);
+		headers.set("Cookie", String.join(";", Objects.requireNonNull(responseEntity.getHeaders().get("Set-Cookie"))));
 
-		/*User sysUser = new User();
-		sysUser.setFirstName("Arvind");
-		sysUser.setLastName("Kuamr");
-		sysUser.setAddress("Noida");
-		sysUser.setGender("M");*/
-		//requestEntity = new HttpEntity<>(sysUser, headers);
+		User sysUser = new User();
+		sysUser.setId(3L);
+		sysUser.setName("James");
+		sysUser.setLastName("Brown");
+		sysUser.setAge((byte) 18);
+		requestEntity = new HttpEntity<>(sysUser, headers);
+		addUserByExchangeMethod(requestEntity);
 
-		//addUserByExchangeMethod(requestEntity);
+		sysUser.setName("Thomas");
+		sysUser.setLastName("Shelby");
+		updateUserByExchangeMethod(requestEntity);
 
-		//updateUserByExchangeMethod(requestEntity);
-
-		//deleteUserByExchangeMethod(requestEntity);
+		deleteUserByExchangeMethod(requestEntity);
 	}
 
-	private static void getListUserByExchangeMethod(HttpEntity<Object> requestEntity) {
+	private static ResponseEntity<List> getListUserByExchangeMethod(HttpEntity<Object> requestEntity) {
 		ResponseEntity<List> responseEntity = restTemplate.exchange(baseUrl,
 				HttpMethod.GET,
 				requestEntity,
 				List.class);
-		HttpStatus statusCode = responseEntity.getStatusCode();
-		System.out.println("status code - " + statusCode);
-		List user = responseEntity.getBody();
-		System.out.println("response body - " + user);
+
 		HttpHeaders responseHeaders = responseEntity.getHeaders();
-		System.out.println("response Headers - " + responseHeaders);
-/*
-        ResponseEntity<User> responseUser = restTemplate.exchange(baseUrl + "user/5",
-                HttpMethod.GET,
-                requestEntity,
-                User.class);
-        User userBody = responseUser.getBody();
-        System.out.println("user object - " + userBody);*/
+		return responseEntity;
+	}
+
+	private static void addUserByExchangeMethod(HttpEntity<Object> requestEntity) {
+		ResponseEntity<String> responseEntity = restTemplate.exchange(baseUrl, HttpMethod.POST, requestEntity, String.class);
+
+		String userDetails = responseEntity.getBody();
+		HttpHeaders responseHeaders = responseEntity.getHeaders();
+	}
+
+	private static void updateUserByExchangeMethod(HttpEntity<Object> requestEntity) {
+		ResponseEntity<String> responseEntity = restTemplate.exchange(baseUrl, HttpMethod.PUT, requestEntity, String.class);
+
+		String userDetails = responseEntity.getBody();
+		HttpHeaders responseHeaders = responseEntity.getHeaders();
+	}
+
+	private static void deleteUserByExchangeMethod(HttpEntity<Object> requestEntity) {
+		ResponseEntity<String> responseEntity = restTemplate.exchange(baseUrl + "/3", HttpMethod.DELETE, requestEntity, String.class);
+
+		String userDetails = responseEntity.getBody();
+		HttpHeaders responseHeaders = responseEntity.getHeaders();
 	}
 }
